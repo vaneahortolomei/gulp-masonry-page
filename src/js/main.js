109403b30252acbc05805ad7522cbb69e6e17fc3
vanea <hortolomei1989@gmail.com>
2021-11-10 12:54:41 +0200

    ГщвUpdate page

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,18 @@
-console.log('main');
-console.log('a');
+const openMenu = document.querySelector('#open-menu');
+const closeMenu = document.querySelector('#close-menu');
+const navMenu = document.querySelector('.nav-menu');
+
+openMenu.addEventListener('click', showMenu);
+closeMenu.addEventListener('click', hideMenu);
+
+function showMenu(e) {
+  e.preventDefault();
+  return navMenu.classList.contains('nav-menu--active') ?
+    navMenu.classList.remove('nav-menu--active') :
+    navMenu.classList.add('nav-menu--active');
+}
+
+function hideMenu(e) {
+  e.preventDefault();
+  return navMenu.classList.remove('nav-menu--active');
+}
